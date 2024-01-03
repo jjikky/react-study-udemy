90bb198a2ab77ab38e53a57c30d4a47c417269c7
jjikky <kok5417@skuniv.ac.kr>
2024-01-03 21:39:29 +0900

    💄 player 컴포넌트 추가

---
+++ tictactoe/src/components/player.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  const handleEditClick = () => {
+    setIsEditing((isEditing) => !isEditing);
+  };
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "save" : "edit"}</button>
+    </li>
+  );
+}
