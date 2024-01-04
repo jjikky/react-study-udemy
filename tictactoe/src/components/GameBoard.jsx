8576c016b2c8692ce354f6e0518d13ea211012a5
jjikky <kok5417@skuniv.ac.kr>
2024-01-04 16:09:35 +0900

    ✨ log컴포넌트 추가 및 리팩토링

--- tictactoe/src/components/GameBoard.jsx
+++ tictactoe/src/components/GameBoard.jsx
@@ -1,40 +1,50 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+	let gameBoard = initialGameBoard;
 
-  const handleSelectSquare = (rowIndex, colIndex) => {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
+	for (const turn of turns) {
+		const { square, player } = turn;
+		const { row, col } = square;
 
-    onSelectSquare();
-  };
-  return (
-    <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
-        <li key={rowIndex}>
-          <ol>
-            {row.map((playerSymbol, colIndex) => (
-              <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
-                  {playerSymbol}
-                </button>
-              </li>
-            ))}
-          </ol>
-        </li>
-      ))}
-    </ol>
-  );
+		gameBoard[row][col] = player;
+	}
+
+	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	// const handleSelectSquare = (rowIndex, colIndex) => {
+	// 	setGameBoard((prevGameBoard) => {
+	// 		const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
+	// 		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+	// 		return updatedBoard;
+	// 	});
+
+	// 	onSelectSquare();
+	// };
+	return (
+		<ol id='game-board'>
+			{gameBoard.map((row, rowIndex) => (
+				<li key={rowIndex}>
+					<ol>
+						{row.map((playerSymbol, colIndex) => (
+							<li key={colIndex}>
+								<button
+									onClick={() => onSelectSquare(rowIndex, colIndex)}
+									disabled={playerSymbol !== null}
+								>
+									{playerSymbol}
+								</button>
+							</li>
+						))}
+					</ol>
+				</li>
+			))}
+		</ol>
+	);
 }
--- tictactoe/src/components/player.jsx
+++ tictactoe/src/components/player.jsx
@@ -1,26 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 export default function Player({ initialName, symbol, isActive }) {
-  const [playerName, setPlayerName] = useState(initialName);
-  const [isEditing, setIsEditing] = useState(false);
-  const handleEditClick = () => {
-    setIsEditing((isEditing) => !isEditing);
-  };
-  const handleChange = (event) => {
-    setPlayerName(event.target.value);
-  };
-  let editablePlayerName = <span className="player-name">{playerName}</span>;
-  if (isEditing) {
-    editablePlayerName = (
-      <input type="text" required value={playerName} onChange={handleChange} />
-    );
-  }
-  return (
-    <li className={isActive ? "active" : undefined}>
-      <span className="player">
-        {editablePlayerName}
-        <span className="player-symbol">{symbol}</span>
-      </span>
-      <button onClick={handleEditClick}>{isEditing ? "save" : "edit"}</button>
-    </li>
-  );
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState(false);
+	const handleEditClick = () => {
+		setIsEditing((isEditing) => !isEditing);
+	};
+	const handleChange = (event) => {
+		setPlayerName(event.target.value);
+	};
+	let editablePlayerName = <span className='player-name'>{playerName}</span>;
+	if (isEditing) {
+		editablePlayerName = <input type='text' required value={playerName} onChange={handleChange} />;
+	}
+	return (
+		<li className={isActive ? 'active' : undefined}>
+			<span className='player'>
+				{editablePlayerName}
+				<span className='player-symbol'>{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{isEditing ? 'save' : 'edit'}</button>
+		</li>
+	);
 }
