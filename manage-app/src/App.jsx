7a128f5ebb2c2ec178272c3005405d465911da3b
jjikky <kok5417@skuniv.ac.kr>
2024-01-10 17:23:44 +0900

    🎨 project State추가

--- manage-app/src/App.jsx
+++ manage-app/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 import ProjectSidebar from './components/ProjectsSidebar';
 
 function App() {
+	const [projectsState, setProjectsState] = useState({
+		selectedProjectId: undefined,
+		projects: [],
+	});
+
+	function handleStartAppProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: null,
+			};
+		});
+	}
+
+	let content;
+
+	if (projectsState.selectedProjectId === null) {
+		content = <NewProject />;
+	} else if (projectsState.selectedProjectId === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStartAppProject} />;
+	}
+
 	return (
 		<main className='h-screen my-8 flex gap-8'>
-			<ProjectSidebar />
-			<NewProject />
+			<ProjectSidebar onStartAddProject={handleStartAppProject} />
+			{content}
 		</main>
 	);
 }
