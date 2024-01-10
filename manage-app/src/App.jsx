e88073fa2ef46cc1a5f014e4c587003b31843d22
jjikky <kok5417@skuniv.ac.kr>
2024-01-10 19:22:54 +0900

    💄 modal component & save 기능 구현

--- manage-app/src/App.jsx
+++ manage-app/src/App.jsx
@@ -18,17 +18,40 @@ function App() {
 		});
 	}
 
+	function handleCancelAddProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+			};
+		});
+	}
+
+	function handleAddProject(projectData) {
+		setProjectsState((prevState) => {
+			const projectId = Math.random();
+			const newProject = {
+				...projectData,
+				id: projectId,
+			};
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+				projects: [...prevState.projects, newProject],
+			};
+		});
+	}
 	let content;
 
 	if (projectsState.selectedProjectId === null) {
-		content = <NewProject />;
+		content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
 	} else if (projectsState.selectedProjectId === undefined) {
 		content = <NoProjectSelected onStartAddProject={handleStartAppProject} />;
 	}
 
 	return (
 		<main className='h-screen my-8 flex gap-8'>
-			<ProjectSidebar onStartAddProject={handleStartAppProject} />
+			<ProjectSidebar onStartAddProject={handleStartAppProject} projects={projectsState.projects} />
 			{content}
 		</main>
 	);
